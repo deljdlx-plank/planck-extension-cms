50fa4c8526b6ee905061c2277fbcdf6537c1282b
Jean luc <you@example.com>
2019-02-05 17:59:47 +0100

    synchro

---
+++ source/class/CMS.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Planck\Extension;
+
+
+use Planck\Application;
+use Planck\Extension;
+
+class CMS extends Extension
+{
+
+
+    public function __construct(Application $application)
+    {
+        parent::__construct($application, 'Planck\Extension\CMS', __DIR__.'/../..');
+
+
+        //$entityEditorExtension =
+
+
+    }
+
+
+}
