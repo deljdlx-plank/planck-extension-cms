673f424ad5e9a332e9bf7dd239b3593c45892c3e
Jean luc <you@example.com>
2019-02-07 09:57:30 +0100

    synchro

--- source/class/Module/SiteTree/Router/Api.php
+++ source/class/Module/SiteTree/Router/Api.php
@@ -6,6 +6,7 @@ namespace Planck\Extension\CMS\Module\SiteTree\Router;
 
 
 use Planck\Extension\CMS\Model\Entity\SiteTree;
+use Planck\Extension\EntityEditor\EntityTreeApiRouter;
 use Planck\Extension\RichTag\Model\Entity\Category;
 use Planck\Extension\ViewComponent\TreeFormater;
 use Planck\Route;
@@ -15,114 +16,22 @@ use Planck\Router;
 
 
 
-class Api extends Router
+class Api extends EntityTreeApiRouter
 {
 
 
-
-
-    public function registerRoutes()
+    public function getEntity()
     {
-
-
-        $this->delete('delete-branch', '`/site-tree/api/delete-branch`', function () {
-            $data = $this->request->getBodyData();
-
-            $category = $this->application->getModelEntity(SiteTree::class);
-            $category->loadById($data['id']);
-
-            $category->deleteBranch(true);
-
-            echo json_encode($category);
-
-        })->json();
-
-
-
-        $this->delete('delete', '`/site-tree/api/delete`', function () {
-            $data = $this->request->getBodyData();
-
-            $category = $this->application->getModelEntity(SiteTree::class);
-            $category->loadById($data['id']);
-            $category->delete();
-            echo json_encode($category);
-        })->json();
-
-
-        $this->post('save', '`/site-tree/api/save`', function() {
-
-            $data = $this->post();
-
-            $parentCategory = $this->application->getModelEntity(SiteTree::class);
-            $parentCategory->loadById($data['parent_id']);
-
-            $newCategory = $this->application->getModelEntity(SiteTree::class);
-            $newCategory->setValues($data);
-            $newCategory->setParent($parentCategory);
-
-            $newCategory->store();
-
-            $formater = new TreeFormater();
-
-            echo json_encode($formater->getNodeFromEntity(
-                $newCategory
-            ));
-
-        })->json();
-
-
-        $this->post('move', '`/site-tree/api/move`', function() {
-
-            /**
-             * @var Route $route
-             */
-            $route = $this->getRouter()->getRouteByName('save');
-            $route->setRequest($this->request);
-            $route->execute();
-            echo $route->getOutput();
-
-        })->json()
-        ;
-
-
-
-        $this->get('get-tree', '`/site-tree/api/get-tree`', function() {
-
-            $repository = $this->application->getModelRepository(\Planck\Extension\CMS\Model\Repository\SiteTree::class);
-            $tree = $repository->getTree(null, 0);
-            $formater = new TreeFormater();
-            echo json_encode(
-                $formater->getTreeFromNodeList($tree)
-            );
-        })->json()
-        ;
-
-        $this->get('initialize', '`/site-tree/api/initialize`', function() {
-
-
-
-            $category = $this->application->getModelEntity(SiteTree::class);
-
-            $root = $category->getRoot(true);
-            if(!$root) {
-                $root = $this->application->getModelEntity(SiteTree::class);
-                $root->setValue('name', 'root');
-                $root->store();
-            }
-            echo json_encode(
-                $root->toExtendedArray()
-            );
-
-
-        })->json();
-
-
-
-
-
-
-
-        return parent::registerRoutes();
+        return $this->application->getModelEntity(SiteTree::class);
     }
 
+    public function getRepository()
+    {
+        return $this->application->getModelRepository(\Planck\Extension\CMS\Model\Repository\SiteTree::class);
+    }
+
+    public function getRoutePath()
+    {
+        return '/site-tree/api';
+    }
 }
